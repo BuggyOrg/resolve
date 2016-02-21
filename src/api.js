8ef89cbae943b36c472616448d3ebbeff005b2ba
Maximilian Klein <dasmaxl@gmail.com>
2016-02-21 22:44:05 +0100

    (chore) Project structure.

---
+++ src/api.js
@@ -0,0 +1,4 @@
+
+export default function resolve (graph, resolve) {
+  return graph
+}
---
+++ test/resolve.js
@@ -0,0 +1,35 @@
+/* global describe, it */
+
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import resolve from '../src/api'
+import graphlib from 'graphlib'
+import gdot from 'graphlib-dot'
+import fs from 'fs'
+import {extname} from 'path'
+import 'babel-register'
+import * as components from './components.json'
+
+chai.use(chaiAsPromised)
+var expect = chai.expect
+
+var readFixture = (file) => {
+  if (extname(file) === '.dot') {
+    return gdot.read(fs.readFileSync('test/fixtures/' + file, 'utf8'))
+  } else {
+    return graphlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/' + file)))
+  }
+}
+
+const resolveFn = (name, version) => {
+  return components[name]
+}
+
+describe('Elastic search meta information interface', () => {
+  it('`resolve` should leave atomics untouched', () => {
+    var atomicsGraph = readFixture('atomic.dot')
+    var resolved = resolve(atomicsGraph, resolveFn)
+    expect(resolved).to.be.an('object')
+    expect(resolved.nodes()).to.have.length(1)
+  })
+})
